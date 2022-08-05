072b7ed40b4a3344c37796df7b8a7a93fc389675
Shahzaib Minhas <shahzaib.minhas@codedistrict.com>
2022-08-05 18:28:26 +0500

    sec15-forms-td-assignment completed

--- sec15-forms/forms-td-assignment-start/src/app/app.component.ts
+++ sec15-forms/forms-td-assignment-start/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  constructor() {}
 
+  @ViewChild("myForm") formData: NgForm;
+
+  subscriptions = ["Basic", "Advanced", "Pro"];
+  isSubmitted: boolean = false;
+
+  onSubmit() {
+    this.isSubmitted = true;
+    console.log("data submitted: ", this.formData.form.value);
+  }
 }
