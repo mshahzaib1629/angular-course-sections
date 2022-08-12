763667fa68b1f69f67f6db29d7f5011e4a79e790
Shahzaib Minhas <shahzaib.minhas@codedistrict.com>
2022-08-12 15:16:37 +0500

    sec15-reactive forms assignment updated

--- sec15-forms/forms-reactive-assignment-start/src/app/app.component.ts
+++ sec15-forms/forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  assignmentForm: FormGroup;
+  projectStatuses = ["Stable", "Critical", "Finished"];
+
+  ngOnInit(): void {
+    this.assignmentForm = new FormGroup({
+      projectName: new FormControl(
+        null,
+        [
+          Validators.required,
+          // this.customValidator,
+        ],
+        this.customValidatorAsync
+      ),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      projectStatus: new FormControl(this.projectStatuses[0]),
+    });
+  }
+
+  onSubmit() {
+    console.log("form values: ");
+    console.log(this.assignmentForm);
+  }
+
+  customValidator(control: FormControl): { [s: string]: boolean } {
+    if (control.value === "Test") {
+      return { forbiddenName: true };
+    } else return null;
+  }
+
+  customValidatorAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === "Test") {
+          resolve({ forbiddenName: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
 }
